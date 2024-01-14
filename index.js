c606becd1655140d147a71822b879bae4c07cb4b
allankimanzi2 <allankim457@gmail.com>
2024-01-14 16:21:20 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    // Your existing code here, if any.
+  
+    const myButton = document.getElementById("myButton");
+    myButton.addEventListener("click", clickAlert);
+  }
+  
+  // Call the function to execute the code.
+  addingEventListener();
+  function addingEventListener() {
 }
